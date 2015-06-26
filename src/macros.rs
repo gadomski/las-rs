f2ab850e754b59ac8b589c20c2d3807a00fdc18e
Pete Gadomski <pete.gadomski@gmail.com>
2015-06-26 14:32:32 -0600

    Export the macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,7 @@
 //! Internal-use macros.
 
 /// Reads n bytes into a buffer, or returns a `ReadError`.
+#[macro_export]
 macro_rules! try_read_n {
     ($reader:expr, $data:expr, $n:expr) => {{
         let took = try!($reader.take($n).read(&mut $data));
