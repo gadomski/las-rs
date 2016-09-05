38734f40770e4fa1381dcd7231ba587de45b559a
Pete Gadomski <pete.gadomski@gmail.com>
2016-09-05 09:29:36 -0600

    Copy example uses writer_from_path

--- examples/copy.rs
+++ examples/copy.rs
@@ -3,7 +3,6 @@
 extern crate las;
 
 use std::env;
-use std::fs::File;
 
 use las::{Builder, Reader};
 
@@ -14,7 +13,7 @@ fn main() {
     let outfile = args.next().expect("Too few arguments");
     let mut reader = Reader::from_path(&infile).expect("Unable to open infile");
     let mut writer = Builder::from_reader(&reader)
-        .writer(File::create(outfile).expect("Unable to open outfile"))
+        .writer_from_path(outfile)
         .expect("Unable to create writer");
     for point in reader.iter_mut() {
         writer.write(&point.expect("Error while reading point"))
