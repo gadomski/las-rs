b6f68c80edef2304d4bb9ed31ea73adb84208ae8
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-23 14:54:38 -0600

    Use quick-error
    Closes #10.

--- src/error.rs
+++ src/error.rs
@@ -1,134 +1,92 @@
 use point::{Format, RawPoint};
-use std::error;
-use std::fmt;
 use std::io;
 use std::str;
 
-/// Crate-specific error enum.
-#[derive(Debug)]
-pub enum Error {
-    /// The writer is closed.
-    ClosedWriter,
-    /// The header size is too small.
-    HeaderSizeTooSmall(u16),
-    /// The las data is laszip compressed.
-    Laszip,
-    /// The offset to data is too small.
-    OffsetToDataTooSmall(u32),
-    /// This format requires color, but it is missing.
-    MissingColor(Format, RawPoint),
-    /// This format requires GPS time, but it is missing.
-    MissingGpsTime(Format, RawPoint),
-    /// This string is not ASCII.
-    NotAscii(String),
-    /// These bytes are not zero-filled.
-    NotZeroFilled(Vec<u8>),
-    /// This is not a valid number of returns.
-    InvalidNumberOfReturns(u8),
-    /// This is not a valid return number.
-    InvalidReturnNumber(u8),
-    /// Wrapper around `std::io::Error`.
-    Io(io::Error),
-    /// The value can't be represnted as an i32.
-    Overflow(f64),
-    /// This string is too long for the target slice.
-    StringTooLong(String, usize),
-    /// Wrapper around `std::str::Utf8Error`.
-    Utf8(str::Utf8Error),
-    /// This version does not support the feature.
-    VersionDoesNotSupport((u8, u8), String),
-    /// The data in the VLR are too long for LAS.
-    VlrDataTooLong(usize),
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
-
-impl From<str::Utf8Error> for Error {
-    fn from(err: str::Utf8Error) -> Error {
-        Error::Utf8(err)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::ClosedWriter => write!(f, "The writer is closed"),
-            Error::HeaderSizeTooSmall(n) => {
-                write!(f, "The header was {} bytes, which is too small", n)
-            }
-            Error::Laszip => write!(f, "The las data is laszip compressed"),
-            Error::OffsetToDataTooSmall(n) => {
-                write!(f, "The offset to data was {} bytes, which is too small", n)
-            }
-            Error::MissingColor(format, _) => {
-                write!(
-                    f,
-                    "Point was missing color information, required by {}",
-                    format
-                )
-            }
-            Error::MissingGpsTime(format, _) => {
-                write!(
-                    f,
-                    "Point was missing GPS time information, required by {}",
-                    format
-                )
-            }
-            Error::NotAscii(ref s) => write!(f, "The string {} is not ASCII", s),
-            Error::NotZeroFilled(ref v) => write!(f, "The vector {:?} was not zero filled", v),
-            Error::InvalidNumberOfReturns(n) => write!(f, "{} is not a valid number of returns", n),
-            Error::InvalidReturnNumber(n) => write!(f, "{} is not a valid return number", n),
-            Error::Overflow(n) => write!(f, "{} cannot be represented as a i32", n),
-            Error::VersionDoesNotSupport(version, ref s) => {
-                write!(
-                    f,
-                    "Version {}.{} does not support {}",
-                    version.0,
-                    version.1,
-                    s
-                )
-            }
-            Error::Io(ref err) => write!(f, "IO error: {}", err),
-            Error::StringTooLong(ref s, n) => {
-                write!(f, "String is too long for a field of {} bytes: {}", n, s)
-            }
-            Error::Utf8(ref err) => write!(f, "Utf8 error: {}", err),
-            Error::VlrDataTooLong(n) => write!(f, "VLR data too long: {} bytes", n),
+quick_error! {
+    /// Crate-specific error enum.
+    #[derive(Debug)]
+    pub enum Error {
+        /// The value can't be represnted as an i32.
+        CannotBeI32(n: f64) {
+            description("the float value cannot be an i32")
+            display("the float value cannot be an i32: {}", n)
         }
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::ClosedWriter => "the writer is closed",
-            Error::HeaderSizeTooSmall(_) => "header size is too small",
-            Error::Laszip => "this library does not support laszip (yet)",
-            Error::OffsetToDataTooSmall(_) => "offset to point data is too small",
-            Error::MissingColor(_, _) => "color data is missing",
-            Error::MissingGpsTime(_, _) => "gps time data is missing",
-            Error::NotAscii(_) => "the string is not ascii",
-            Error::NotZeroFilled(_) => "the string is not zero filled",
-            Error::InvalidNumberOfReturns(_) => "invalid number of returns",
-            Error::InvalidReturnNumber(_) => "invalid return number",
-            Error::Io(ref err) => err.description(),
-            Error::Overflow(_) => "number cannot be represented as an i32",
-            Error::StringTooLong(_, _) => "string is too long",
-            Error::Utf8(ref err) => err.description(),
-            Error::VersionDoesNotSupport(_, _) => "version does not support feature",
-            Error::VlrDataTooLong(_) => "vlr data is too long",
+        /// The writer is closed.
+        ClosedWriter {
+            description("the writer is closed")
         }
-    }
-
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            Error::Io(ref err) => Some(err),
-            Error::Utf8(ref err) => Some(err),
-            _ => None,
+        /// The header size is too small.
+        HeaderSizeTooSmall(header_size: u16) {
+            description("the header size is too small")
+            display("the header size is too small: {}", header_size)
+        }
+        /// The las data is laszip compressed.
+        Laszip {
+            description("the las data is laszip compressed, and laszip compression is not supported by this build")
+        }
+        /// The offset to data is too small.
+        OffsetToDataTooSmall(offset: u32) {
+            description("the offset to the data is too small")
+            display("the offset to the data is too small: {}", offset)
+        }
+        /// This format requires color, but it is missing.
+        MissingColor(format: Format, point: RawPoint) {
+            description("the point format requires color, but it is missing")
+            display("point format {} requires color, but the point doesn't have color", format)
+        }
+        /// This format requires GPS time, but it is missing.
+        MissingGpsTime(format: Format, point: RawPoint) {
+            description("the point format requires gps time, but it is missing")
+            display("point format {} requires gps time, but the point doesn't have gps time", format)
+        }
+        /// This string is not ASCII.
+        NotAscii(s: String) {
+            description("the string is not an ascii string")
+            display("this string is not ascii: {}", s)
+        }
+        /// These bytes are not zero-filled.
+        NotZeroFilled(bytes: Vec<u8>) {
+            description("the bytes are not zero filled")
+            display("the bytes are not zero filled: {:?}", bytes)
+        }
+        /// This is not a valid number of returns.
+        InvalidNumberOfReturns(n: u8) {
+            description("invalid number of returns")
+            display("the number of returns is invalid: {}", n)
+        }
+        /// This is not a valid return number.
+        InvalidReturnNumber(n: u8) {
+            description("invalid return number")
+            display("the return number is invalid: {}", n)
+        }
+        /// Wrapper around `std::io::Error`.
+        Io(err: io::Error) {
+            from()
+            cause(err)
+            description(err.description())
+            display("io error: {}", err)
+        }
+        /// This string is too long for the target slice.
+        StringTooLong(s: String, len: usize) {
+            description("the string is too long for the target slice")
+            display("string is too long for a slice of length {}: {}", len, s)
+        }
+        /// Wrapper around `std::str::Utf8Error`.
+        Utf8(err: str::Utf8Error) {
+            from()
+            cause(err)
+            description(err.description())
+            display("utf8 error: {}", err)
+        }
+        /// This version does not support the feature.
+        VersionDoesNotSupport(version: (u8, u8), s: String) {
+            description("las version does not support the provided feature")
+            display("las version {}.{} does not support feature: {}", version.0, version.1, s)
+        }
+        /// The data in the VLR are too long for LAS.
+        VlrDataTooLong(n: usize) {
+            description("the data in the vlr are too long for las")
+            display("vlr data of length {} are too long", n)
         }
     }
 }
