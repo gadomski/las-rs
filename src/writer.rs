6eb29125d4ffd8fbcc5029fe41948e2b71f350e3
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-28 07:12:52 -0600

    Check for missing point attributes when writing

--- src/writer.rs
+++ src/writer.rs
@@ -18,10 +18,30 @@ quick_error! {
             description("format is not supported by version")
             display("format {} is not supported by version {}", format, version)
         }
+        /// The point format has color, but the point doesn't.
+        MissingColor(format: Format, point: Point) {
+            description("the point format has color, but the point doesn't")
+            display("point format {} has color, but point {:?} doesn't", format, point)
+        }
         /// The point format has extra bytes, but the point doesn't.
         MissingExtraBytes(format: Format, point: Point) {
             description("the point format has extra bytes, but the point doesn't")
             display("point format {} has extra bytes, but point {:?} doesn't", format, point)
+        }
+        /// The point format has gps time, but the point doesn't.
+        MissingGpsTime(format: Format, point: Point) {
+            description("the point format has gps time, but the point doesn't")
+            display("point format {} has gps time, but point {:?} doesn't", format, point)
+        }
+        /// The point format has nir, but the point doesn't.
+        MissingNir(format: Format, point: Point) {
+            description("the point format has nir, but the point doesn't")
+            display("point format {} has nir, but point {:?} doesn't", format, point)
+        }
+        /// The point format has waveform, but the point doesn't.
+        MissingWaveform(format: Format, point: Point) {
+            description("the point format has waveform, but the point doesn't")
+            display("point format {} has waveform, but point {:?} doesn't", format, point)
         }
     }
 }
@@ -117,6 +137,22 @@ impl<W: Seek + Write> Writer<W> {
         if self.closed {
             return Err(Error::Closed.into());
         }
+        if self.header.point_format.has_gps_time && point.gps_time.is_none() {
+            return Err(
+                Error::MissingGpsTime(self.header.point_format, point).into(),
+            );
+        }
+        if self.header.point_format.has_color && point.color.is_none() {
+            return Err(Error::MissingColor(self.header.point_format, point).into());
+        }
+        if self.header.point_format.has_nir && point.nir.is_none() {
+            return Err(Error::MissingNir(self.header.point_format, point).into());
+        }
+        if self.header.point_format.has_waveform && point.waveform.is_none() {
+            return Err(
+                Error::MissingWaveform(self.header.point_format, point).into(),
+            );
+        }
         if self.header.point_format.extra_bytes as usize != point.extra_bytes.len() {
             return Err(
                 Error::MissingExtraBytes(self.header.point_format, point).into(),
@@ -197,13 +233,22 @@ impl<W: Seek + Write> Drop for Writer<W> {
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
     use Header;
-
     use byteorder::{LittleEndian, ReadBytesExt};
-
+    use point::Format;
     use std::io::Cursor;
+
+    fn writer(format: Format, version: Version) -> Writer<Cursor<Vec<u8>>> {
+        Writer::new(
+            Cursor::new(Vec::new()),
+            Header {
+                point_format: format,
+                version: version,
+                ..Default::default()
+            },
+        ).unwrap()
+    }
 
     #[test]
     fn las_1_0_point_data_start_signature() {
@@ -222,26 +267,52 @@ mod tests {
     }
 
     #[test]
-    fn writer_cant_write_closed() {
+    fn already_closed() {
         let mut writer = Writer::new(Cursor::new(Vec::new()), Default::default()).unwrap();
         writer.close().unwrap();
         assert!(writer.write(Default::default()).is_err());
     }
 
     #[test]
-    fn write_point_with_incorrect_extra_bytes() {
-        use point::Format;
+    fn missing_extra_bytes() {
         let format = Format {
             extra_bytes: 1,
             ..Default::default()
         };
-        let mut writer = Writer::new(
-            Cursor::new(Vec::new()),
-            Header {
-                point_format: format,
-                ..Default::default()
-            },
-        ).unwrap();
+        let mut writer = writer(format, Version::new(1, 4));
+        assert!(writer.write(Default::default()).is_err());
+    }
+
+    #[test]
+    fn missing_gps_time() {
+        let format = Format::new(1).unwrap();
+        let mut writer = writer(format, Version::new(1, 2));
+        assert!(writer.write(Default::default()).is_err());
+    }
+
+    #[test]
+    fn missing_color() {
+        let format = Format::new(2).unwrap();
+        let mut writer = writer(format, Version::new(1, 2));
+        assert!(writer.write(Default::default()).is_err());
+    }
+
+    #[test]
+    fn missing_nir() {
+        let format = Format::new(8).unwrap();
+        let mut writer = writer(format, Version::new(1, 4));
+        let point = Point {
+            gps_time: Some(0.),
+            color: Some(Default::default()),
+            ..Default::default()
+        };
+        assert!(writer.write(point).is_err());
+    }
+
+    #[test]
+    fn missing_waveform() {
+        let format = Format::new(4).unwrap();
+        let mut writer = writer(format, Version::new(1, 4));
         assert!(writer.write(Default::default()).is_err());
     }
 }
