9659d6ea4151abe9428ca426f0fa191b1e36b056
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-30 12:51:59 -0600

    Re-indent roundtrip macro

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -109,92 +109,90 @@ macro_rules! roundtrip_header {
     };
 }
 
-macro_rules! version {
-    ($name:ident, $major:expr, $minor:expr) => {
-        mod $name {
-            use las::Version;
+macro_rules! version { ($name:ident, $major:expr, $minor:expr) => {
+mod $name {
+    use las::Version;
 
-            fn version() -> Version {
-                Version::new($major, $minor)
-            }
+    fn version() -> Version {
+        Version::new($major, $minor)
+    }
 
-            mod point {
-                use las::Color;
-                use las::point::{Classification, ScanDirection};
+    mod point {
+        use las::Color;
+        use las::point::{Classification, ScanDirection};
 
-                roundtrip_point!(xyz, |p: &mut Point| { p.x = 1.; p.y = 2.; p.z = 3.; });
-                roundtrip_point!(intensity, |p: &mut Point| p.intensity = 42);
-                roundtrip_point!(return_number, |p: &mut Point| p.return_number = 2);
-                roundtrip_point!(number_of_returns, |p: &mut Point| p.number_of_returns = 2);
-                roundtrip_point!(scan_direction, |p: &mut Point| p.scan_direction = ScanDirection::LeftToRight);
-                roundtrip_point!(is_edge_of_flight_line, |p: &mut Point| p.is_edge_of_flight_line = true);
-                roundtrip_point!(classification, |p: &mut Point| p.classification = Classification::Ground);
-                roundtrip_point!(is_synthetic, |p: &mut Point| p.is_synthetic = true);
-                roundtrip_point!(is_key_point, |p: &mut Point| p.is_key_point = true);
-                roundtrip_point!(is_withheld, |p: &mut Point| p.is_withheld = true);
-                roundtrip_point!(is_overlap, |p: &mut Point| {
-                    p.classification = Classification::Unclassified;
-                    p.is_overlap = true;
-                });
-                roundtrip_point!(scanner_channel, |p: &mut Point| {
-                    p.scanner_channel = 1;
-                    p.gps_time = Some(42.);
-                }, 4, |f: &mut Format| f.extend());
-                roundtrip_point!(scan_angle_rank, |p: &mut Point| p.scan_angle = 3.);
-                roundtrip_point!(user_data, |p: &mut Point| p.user_data = 42);
-                roundtrip_point!(point_source_id, |p: &mut Point| p.point_source_id = 42);
-                roundtrip_point!(gps_time, |p: &mut Point| p.gps_time = Some(42.), 0, |f: &mut Format| f.has_gps_time = true);
-                roundtrip_point!(color, |p: &mut Point| p.color = Some(Color { red: 1, green: 2, blue: 3 }), 2, |f: &mut Format| f.has_color = true);
-                // TODO waveform
-                roundtrip_point!(nir, |p: &mut Point| {
-                    p.color = Some(Color { red: 1, green: 2, blue: 3});
-                    p.nir = Some(42);
-                    p.gps_time = Some(42.);
-                }, 4, |f: &mut Format| {
-                    f.extend();
-                    f.has_color = true;
-                    f.has_nir = true;
-                });
-                roundtrip_point!(extra_bytes, |p: &mut Point| p.extra_bytes = vec![42], 0, |f: &mut Format| f.extra_bytes = 1);
-            }
+        roundtrip_point!(xyz, |p: &mut Point| { p.x = 1.; p.y = 2.; p.z = 3.; });
+        roundtrip_point!(intensity, |p: &mut Point| p.intensity = 42);
+        roundtrip_point!(return_number, |p: &mut Point| p.return_number = 2);
+        roundtrip_point!(number_of_returns, |p: &mut Point| p.number_of_returns = 2);
+        roundtrip_point!(scan_direction, |p: &mut Point| p.scan_direction = ScanDirection::LeftToRight);
+        roundtrip_point!(is_edge_of_flight_line, |p: &mut Point| p.is_edge_of_flight_line = true);
+        roundtrip_point!(classification, |p: &mut Point| p.classification = Classification::Ground);
+        roundtrip_point!(is_synthetic, |p: &mut Point| p.is_synthetic = true);
+        roundtrip_point!(is_key_point, |p: &mut Point| p.is_key_point = true);
+        roundtrip_point!(is_withheld, |p: &mut Point| p.is_withheld = true);
+        roundtrip_point!(is_overlap, |p: &mut Point| {
+            p.classification = Classification::Unclassified;
+            p.is_overlap = true;
+        });
+        roundtrip_point!(scanner_channel, |p: &mut Point| {
+            p.scanner_channel = 1;
+            p.gps_time = Some(42.);
+        }, 4, |f: &mut Format| f.extend());
+        roundtrip_point!(scan_angle_rank, |p: &mut Point| p.scan_angle = 3.);
+        roundtrip_point!(user_data, |p: &mut Point| p.user_data = 42);
+        roundtrip_point!(point_source_id, |p: &mut Point| p.point_source_id = 42);
+        roundtrip_point!(gps_time, |p: &mut Point| p.gps_time = Some(42.), 0, |f: &mut Format| f.has_gps_time = true);
+        roundtrip_point!(color, |p: &mut Point| p.color = Some(Color { red: 1, green: 2, blue: 3 }), 2, |f: &mut Format| f.has_color = true);
+        // TODO waveform
+        roundtrip_point!(nir, |p: &mut Point| {
+            p.color = Some(Color { red: 1, green: 2, blue: 3});
+            p.nir = Some(42);
+            p.gps_time = Some(42.);
+        }, 4, |f: &mut Format| {
+            f.extend();
+            f.has_color = true;
+            f.has_nir = true;
+        });
+        roundtrip_point!(extra_bytes, |p: &mut Point| p.extra_bytes = vec![42], 0, |f: &mut Format| f.extra_bytes = 1);
+    }
 
-            mod header {
-                use chrono::{Utc, TimeZone};
-                use las::GpsTimeType;
+    mod header {
+        use chrono::{Utc, TimeZone};
+        use las::GpsTimeType;
 
-                roundtrip_header!(file_source_id, |h: &mut Header| h.file_source_id = 42, 1);
-                roundtrip_header!(gps_time_type, |h: &mut Header| h.gps_time_type = GpsTimeType::Standard, 2);
-                roundtrip_header!(guid, |h: &mut Header| h.guid = [42; 16]);
-                roundtrip_header!(system_identifier, |h: &mut Header| h.system_identifier = "roundtrip test".to_string());
-                roundtrip_header!(generating_software, |h: &mut Header| h.generating_software = "roundtrip test".to_string());
-                roundtrip_header!(date, |h: &mut Header| h.date = Some(Utc.ymd(2017, 10, 30)));
-                roundtrip_header!(padding, |h: &mut Header| h.padding = b"You probably shouldn't do this".to_vec());
-                roundtrip_header!(vlr_padding, |h: &mut Header| h.vlr_padding = b"You probably shouldn't do this either".to_vec());
-                roundtrip_header!(transforms, |h: &mut Header| {
-                    use las::{Transform, Vector};
+        roundtrip_header!(file_source_id, |h: &mut Header| h.file_source_id = 42, 1);
+        roundtrip_header!(gps_time_type, |h: &mut Header| h.gps_time_type = GpsTimeType::Standard, 2);
+        roundtrip_header!(guid, |h: &mut Header| h.guid = [42; 16]);
+        roundtrip_header!(system_identifier, |h: &mut Header| h.system_identifier = "roundtrip test".to_string());
+        roundtrip_header!(generating_software, |h: &mut Header| h.generating_software = "roundtrip test".to_string());
+        roundtrip_header!(date, |h: &mut Header| h.date = Some(Utc.ymd(2017, 10, 30)));
+        roundtrip_header!(padding, |h: &mut Header| h.padding = b"You probably shouldn't do this".to_vec());
+        roundtrip_header!(vlr_padding, |h: &mut Header| h.vlr_padding = b"You probably shouldn't do this either".to_vec());
+        roundtrip_header!(transforms, |h: &mut Header| {
+            use las::{Transform, Vector};
 
-                    let transform = Transform { scale: 0.1, offset: -1. };
-                    h.transforms = Vector {
-                        x: transform,
-                        y: transform,
-                        z: transform,
-                    };
-                });
-                roundtrip_header!(vlrs, |h: &mut Header| h.vlrs = vec![Default::default()]);
-                roundtrip_header!(evlrs, |h: &mut Header| {
-                    use std::u16;
-                    use las::Vlr;
+            let transform = Transform { scale: 0.1, offset: -1. };
+            h.transforms = Vector {
+                x: transform,
+                y: transform,
+                z: transform,
+            };
+        });
+        roundtrip_header!(vlrs, |h: &mut Header| h.vlrs = vec![Default::default()]);
+        roundtrip_header!(evlrs, |h: &mut Header| {
+            use std::u16;
+            use las::Vlr;
 
-                    let mut vlr = Vlr::default();
-                    vlr.is_extended = true;
-                    vlr.data = vec![42; u16::MAX as usize + 1];
-                    h.vlrs = vec![vlr];
-                }, 4);
-                roundtrip_header!(has_synthetic_return_numbers, |h: &mut Header| h.has_synthetic_return_numbers = true, 3);
-            }
-        }
+            let mut vlr = Vlr::default();
+            vlr.is_extended = true;
+            vlr.data = vec![42; u16::MAX as usize + 1];
+            h.vlrs = vec![vlr];
+        }, 4);
+        roundtrip_header!(has_synthetic_return_numbers, |h: &mut Header| h.has_synthetic_return_numbers = true, 3);
     }
 }
+}}
 
 version!(las_1_0, 1, 0);
 version!(las_1_1, 1, 1);
