c15d1f89c9b73475eca9831e240256e22e1f8c2e
Pete Gadomski <pete.gadomski@gmail.com>
2017-11-13 07:13:18 -0700

    Add an example of counting points

---
+++ examples/count.rs
@@ -0,0 +1,17 @@
+//! Counts the number of points in a las file.
+
+extern crate las;
+
+use las::Reader;
+
+fn main() {
+    let path = std::env::args().skip(1).next().expect(
+        "Must provide a path to a las file",
+    );
+    let mut reader = Reader::from_path(path).expect("Unable to open reader");
+    let npoints = reader
+        .points()
+        .map(|p| p.expect("Unable to read point"))
+        .count();
+    println!("Number of points: {}", npoints);
+}
